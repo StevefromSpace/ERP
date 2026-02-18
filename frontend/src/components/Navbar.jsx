4b9e588ded98b010551c4b1563ee54bf9bacbaee
StevefromSpace <iamstevefromspace@gmail.com>
2026-02-18 15:23:24 +0530

    changed the style and improved the presentation of the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,21 +2,21 @@ import  {Link} from 'react-router-dom';
 
 export default function Navbar(){
     return(
-        <nav className="flex justify-between items-center p-4 bg-[#111827] text-white border-b border-gray-800 w-full">
+        <nav className="flex justify-between items-center px-8 py-3 bg-[#1a1c1e] text-white border-b border-gray-800 shadow-lg">
             
-            <div className="flex item-center gap-3">
+            <div className="flex items-center gap-3">
                 <span className=" text-2xl">🚀</span>
-                <Link to="/" className="text-xl font-bold tracking-tight">
+                <Link to="/" className="text-xl font-bold tracking-tight hover:opacity-90 transition-opacity">
                         Epic Proportions
                 </Link>
             </div>
 
-            <div className="flex items-center gap-16 text-gray-400">
-                <Link to="/         " className="hover:text-white flex items-center gap-2">Dashboard</Link>
-                <Link to="/         " className="hover:text-white flex items-center gap-2">Admin Dashboard</Link>
+            <div className="flex items-center gap-10">
+                <Link to="/         " className="flex items-center gap-2 text-gray-400 hover:text-white transition-colors text-sm font-medium">Dashboard</Link>
+                <Link to="/         " className="flex items-center gap-2 text-gray-400 hover:text-white transition-colors text-sm font-medium">Admin Dashboard</Link>
             </div>
 
-            <Link to="/login" className="border border-gray-500 px-5 py-2 rounded-md hover:bg-gray-800 transition">Login</Link>
+            <Link to="/login" className="flex items-center gap-2 px-4 py-1.5 border border-gray-600 rounded-md text-sm font-medium hover:bg-white hover:text-black transition-all duration-300">Login</Link>
         </nav>
     );
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
 export default function Dashboard() {
   return (
-    <div className="Flex">
-        <h1>Hello</h1>
+    <div className="Flex bg-gray-50">
+        <h1>Teacher Dashboard</h1>
+        <h2>WElcome to Team Nasa ERP</h2>
     </div>
   );
 }
