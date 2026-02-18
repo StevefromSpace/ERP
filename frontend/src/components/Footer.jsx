d83b04622d43f8a6b6276ac75b022669ec7edf01
StevefromSpace <iamstevefromspace@gmail.com>
2026-02-18 21:19:43 +0530

    Implemented Footer design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,13 @@
 export default function Footer(){
     return(
-        <footer className="flex justify-between p-4 text-white">
-
-            <h1>Test</h1>
+        <footer className="flex justify-center items-center p-6 bg-[#111827] text-gray-400 border-t border-gray-800 w-full text-s">
+            <div>
+                &copy; 2026 <strong className="text-white"> &rarr; Epic Proportion</strong> | ERP System v0.0 | &nbsp;
+            </div>
+            <div className="flex items-center gap-2">
+                <span className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></span>
+                <span className="font-medium">  System Normal </span>
+            </div>
         </footer>
     );
 }
