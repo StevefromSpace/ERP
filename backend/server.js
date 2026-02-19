84084043a721c30c8608e2b65c4dccf93f2fb648
StevefromSpace <iamstevefromspace@gmail.com>
2026-02-19 13:56:24 +0530

    Initial Backend files

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const cors=require('cors');
+require('dotenv').config();
+
+const app=express();
+const PORT=5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send("ERP is online!");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`)   
+});
